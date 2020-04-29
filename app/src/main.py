0a739ee0c31de0e03ab2fa399c9a5d3b78a4a605
Daniel Valcarce <davamix@gmail.com>
2020-04-29 13:24:58 +0200

    Added notes section only visible after search. Refactor Post data

--- app/src/main.py
+++ app/src/main.py
@@ -10,19 +10,21 @@ templates_path = Path(Path.cwd(), "src", "templates")
 
 app = Flask(__name__, template_folder=templates_path)
 
-# Testing db connection
-client_db = MongoClient("mongodb://mongodb:27017")
-database = client_db.papers_db
-papers = database.papers_collection
-paper = {
-    "paper_id": "1111.1111",
-    "title": "Paper title",
-    "abstract": "A lot of words in the abstract",
-    "authors": "My cat",
-    "link_pdf": "http://server.com/paper"
-}
-paper_mongo_id = papers.insert_one(paper).inserted_id
-print(f"## INSERTED PAPER: {paper_mongo_id}")
+## Testing db connection
+# client_db = MongoClient("mongodb://mongodb:27017") # mongodb://service_name_in_docker_compose:27017
+# database = client_db.papers_db
+# papers = database.papers_collection
+# print(f"## All papers: {papers.count_documents({})}")
+# paper = {
+#     "paper_id": "1111.1111",
+#     "title": "Paper title",
+#     "abstract": "A lot of words in the abstract",
+#     "authors": "My cat",
+#     "link_pdf": "http://server.com/paper"
+# }
+# paper_mongo_id = papers.insert_one(paper).inserted_id
+# print(f"## INSERTED PAPER: {paper_mongo_id}")
+# print(papers.count_documents({}))
 
 
 # https://stackoverflow.com/a/45438226/844372
